47d9fc792be37ffa97a4e425516efd0fbb564ea0
DerSepl1 <s.pietsch60@gmail.com>
2026-07-04 19:21:18 +0200

    Update config_flow.py

--- custom_components/netzooe_smartmeter/config_flow.py
+++ custom_components/netzooe_smartmeter/config_flow.py
@@ -1,26 +1,15 @@
 import voluptuous as vol
 from homeassistant import config_entries
-from homeassistant.core import HomeAssistant
+from .const import DOMAIN
 
-# Importiere deine DOMAIN aus der const.py
-from .const import DOMAIN 
-
-# WICHTIG: Das 'domain=DOMAIN' am Ende der Zeile darf nicht fehlen!
 class NetzOOeConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
-    """Handle a config flow for Netz OÖ Smart Meter."""
-
     VERSION = 1
 
     async def async_step_user(self, user_input=None):
-        """Handle the initial step."""
         errors = {}
-
         if user_input is not None:
-            # Hier passiert später dein Login-Test
-            # Wenn alles klappt:
             return self.async_create_entry(title="Netz OÖ", data=user_input)
 
-        # Das Formular, das in Home Assistant angezeigt wird
         data_schema = vol.Schema({
             vol.Required("username"): str,
             vol.Required("password"): str,
