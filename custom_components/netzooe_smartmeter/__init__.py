12504bf6416f909689c641126df009534d33242d
DerSepl1 <s.pietsch60@gmail.com>
2026-07-04 17:48:04 +0200

    Update __init__.py

--- custom_components/netzooe_smartmeter/__init__.py
+++ custom_components/netzooe_smartmeter/__init__.py
@@ -1,6 +1,13 @@
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 
+from .const import (
+    DOMAIN,
+    CONF_USERNAME,
+    CONF_PASSWORD,
+)
+from .coordinator import NetzOOECoordinator
+
 PLATFORMS = ["sensor"]
 
 
@@ -8,10 +15,38 @@ async def async_setup(hass: HomeAssistant, config):
     return True
 
 
-async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
-    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
+async def async_setup_entry(
+    hass: HomeAssistant,
+    entry: ConfigEntry,
+):
+
+    coordinator = NetzOOECoordinator(
+        hass,
+        entry.data[CONF_USERNAME],
+        entry.data[CONF_PASSWORD],
+    )
+
+    await coordinator.async_config_entry_first_refresh()
+
+    hass.data.setdefault(DOMAIN, {})
+    hass.data[DOMAIN][entry.entry_id] = coordinator
+
+    await hass.config_entries.async_forward_entry_setups(
+        entry,
+        PLATFORMS,
+    )
+
     return True
 
 
-async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
-    return await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
+async def async_unload_entry(
+    hass: HomeAssistant,
+    entry: ConfigEntry,
+):
+
+    await hass.config_entries.async_unload_platforms(
+        entry,
+        PLATFORMS,
+    )
+
+    return True
