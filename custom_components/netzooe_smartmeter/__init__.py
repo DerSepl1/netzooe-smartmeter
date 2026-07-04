9e6fa87b7205656fbbee3ce443616305f498a8c3
DerSepl1 <s.pietsch60@gmail.com>
2026-07-04 19:21:28 +0200

    Update __init__.py

--- custom_components/netzooe_smartmeter/__init__.py
+++ custom_components/netzooe_smartmeter/__init__.py
@@ -1,12 +1,11 @@
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
-
 from .const import DOMAIN, CONF_USERNAME, CONF_PASSWORD
 from .coordinator import NetzOOeDataUpdateCoordinator
 
 PLATFORMS = ["sensor"]
 
-async def async_setup(hass: HomeAssistant, config):
+async def async_setup(hass: HomeAssistant, config: dict):
     return True
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
@@ -15,7 +14,6 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
         entry.data[CONF_USERNAME],
         entry.data[CONF_PASSWORD],
     )
-
     await coordinator.async_config_entry_first_refresh()
 
     hass.data.setdefault(DOMAIN, {})
@@ -25,9 +23,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
     return True
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
-    await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
-    
-    if not hass.data[DOMAIN]:
-        hass.data.pop(DOMAIN)
-        
-    return True
+    unload_ok = await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
+    if unload_ok:
+        hass.data[DOMAIN].pop(entry.entry_id)
+    return unload_ok
