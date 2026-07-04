1da2e369b970927eb6027637ab136d30e3cad490
DerSepl1 <s.pietsch60@gmail.com>
2026-07-04 17:52:05 +0200

    Update sensor.py

--- custom_components/netzooe_smartmeter/sensor.py
+++ custom_components/netzooe_smartmeter/sensor.py
@@ -1,4 +1,5 @@
 from homeassistant.components.sensor import SensorEntity
+from homeassistant.helpers.update_coordinator import CoordinatorEntity
 
 from .const import DOMAIN
 
@@ -8,37 +9,56 @@ async def async_setup_entry(
     entry,
     async_add_entities,
 ):
-
     coordinator = hass.data[DOMAIN][entry.entry_id]
 
     async_add_entities(
         [
-            NetzOOERawSensor(coordinator),
+            NetzOOELastValueSensor(coordinator),
+            NetzOOELastTimestampSensor(coordinator),
         ]
     )
 
 
-class NetzOOERawSensor(SensorEntity):
-
-    _attr_name = "Netz OÖ Rohdaten"
-
-    _attr_unique_id = "netzooe_raw"
+class NetzOOEBaseSensor(CoordinatorEntity, SensorEntity):
 
     def __init__(self, coordinator):
-        self.coordinator = coordinator
+        super().__init__(coordinator)
+
+
+class NetzOOELastValueSensor(NetzOOEBaseSensor):
+
+    _attr_name = "Netz OÖ Letzter Verbrauch"
+    _attr_unique_id = "netzooe_last_value"
+    _attr_native_unit_of_measurement = "kWh"
 
     @property
     def native_value(self):
 
-        if self.coordinator.data is None:
-            return None
-
-        return len(
-            self.coordinator.data.get(
-                "profileValues",
-                [],
-            )
+        values = self.coordinator.data.get(
+            "profileValues",
+            [],
         )
 
-    async def async_update(self):
-        await self.coordinator.async_request_refresh()
+        if not values:
+            return None
+
+        return values[-1]["value"]
+
+
+class NetzOOELastTimestampSensor(NetzOOEBaseSensor):
+
+    _attr_name = "Netz OÖ Letzte Messung"
+    _attr_unique_id = "netzooe_last_timestamp"
+
+    @property
+    def native_value(self):
+
+        values = self.coordinator.data.get(
+            "profileValues",
+            [],
+        )
+
+        if not values:
+            return None
+
+        return values[-1]["datetime"]
