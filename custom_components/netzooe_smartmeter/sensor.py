5496bf6183608d181a5d94783fa62aca85e4e31c
DerSepl1 <s.pietsch60@gmail.com>
2026-07-04 17:47:25 +0200

    Update sensor.py

--- custom_components/netzooe_smartmeter/sensor.py
+++ custom_components/netzooe_smartmeter/sensor.py
@@ -1,20 +1,21 @@
 from homeassistant.components.sensor import SensorEntity
 
-from .const import DOMAIN
-
 
 async def async_setup_entry(hass, entry, async_add_entities):
+
     async_add_entities(
         [
-            NetzOOETestSensor(),
+            NetzOOERawSensor(),
         ]
     )
 
 
-class NetzOOETestSensor(SensorEntity):
-    _attr_name = "Netz OÖ Test"
-    _attr_unique_id = "netzooe_test"
+class NetzOOERawSensor(SensorEntity):
+
+    _attr_name = "Netz OÖ Rohdaten"
+
+    _attr_unique_id = "netzooe_raw"
 
     @property
     def native_value(self):
-        return "Integration geladen"
+        return "API bereit"
